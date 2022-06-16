218823778a18aa6fb9fc3ef9c8cd2518a3aee2cb
JinseonLee <memilmuk82@gmail.com>
2022-06-16 13:25:01 +0900

    noom_websocket

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,3 +1,20 @@
 // alert("hi!");
 // const socket = new WebSocket("http://localhost:3000");
 const socket = new WebSocket(`ws://${window.location.host}`);
+
+socket.addEventListener("open", () => {
+    console.log("Connect to Server")
+}); // "open" 이벤트 -> 서버에 연결
+
+socket.addEventListener("message", (message) => {
+    console.log("Just got this:", message.data, "from the server") 
+    // message 객체의 data속성 -> 전달된 메시지 반환
+}); // "message" 이벤트 -> 메시지 전달 : 메시지를 전달 받아서 콘솔에 출력
+
+socket.addEventListener("close", () => {
+    console.log("Disconnected from Server")
+}); // "close" 이벤트 -> 서버 오프라인 상태
+
+setTimeout(() => {
+    socket.send("hello from browser!");
+}, 5000); // setTimeout 메소드 -> 5000밀리초(5초) 후에 사용자에서 서버에게 메시지 전송
--- src/server.js
+++ src/server.js
@@ -18,10 +18,28 @@ const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 // 웹 소켓 서버를 생성하면서 여기에 HTTP서버를 전달
 
-function handleConnection(socket){
-    console.log(socket)
-};
+/* function handleConnection(socket){
+    console.log(socket);
+}; 
+// wss의 on메소드에서 전달한 socket을 받기 위해 매개변수(socket)를 정의함.
+// socket -> 사용자와 서버 사이의 연결 또는 그에 대한 정보를 의미
+// console.log(socket) -> socket의 내용 확인
 
 wss.on("connection", handleConnection);
+// 웹 소켓 서버 wss의 on 메소드 -> "connetcion" 이벤트 발생시 -> 콜백 함수 handleConnection 호출
+// on 메소드는 콜백 함수에 socket을 전달
+*/
+
+wss.on("connection", (socket) => {
+   // console.log(socket);
+   console.log("Connected to Browser"); 
+   socket.on("close", () => console.log("Disconnected from Browser")); // close 이벤트 발생 -> 서버쪽 콘솔에 메시지 출력
+   socket.on("message", (message) => { // message 이벤트 -> 콜백 함수는 message를 전달받아 콘솔에 출력
+    console.log(`${message}`);
+   });
+   socket.send("hello!!"); // socket의 send 메소드로 메시지 전달, 서버 -> 사용자(프런트엔드)
+}); 
+// handleConection 함수를 선언한 다음에 on 메소드에 인자로 전달 -> 같은 역할을 하는 익명함수를 만들어 on 메소드에 포함
+// connection 이벤트가 발생했을 때 소켓을 받는 다는 것을 직관적으로 표시
 
 server.listen(3000, handleListen);
