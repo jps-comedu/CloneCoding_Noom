a404bcc26c322a0b9ef241353bccb07163c64a91
JinseonLee <memilmuk82@gmail.com>
2022-07-08 21:31:58 +0900

    noom_socket.io

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,48 +1,18 @@
-const messageList = document.querySelector("ul"); 
-// querySelector 메소드 : 목록과 입력 폼을 각각 선택 후 적절한 이름 부여
-const nickForm = document.querySelector("#nick");
-const messageForm = document.querySelector("#message");
-const socket = new WebSocket(`ws://${window.location.host}`);
+const socket = io(); // io() -> socket.io를 실행하고 있는 서버 탐색
+// socket.io가 웹 소켓을 이용해 기능 수행
 
-function makeMessage(type, payload){ // 입력값 유형, 입력값을 인자로 전달 받음 -> JSON -> 문자열로 변환
-    const msg = { type, payload };
-    return JSON.stringify(msg); // JSON.stringify 메서드 -> JSON을 문자열로 변환
-}
+const welcome = document.getElementById("welcome");
+const form = welcome.querySelector("form");
 
-socket.addEventListener("open", () => {
-    console.log("Connect to Server")
-}); // "open" 이벤트 -> 서버에 연결
-
-socket.addEventListener("message", (message) => {
-    const li = document.createElement("li"); // 메시지가 전달될 때마다 새로운 Element 객체(li요소) 생성
-    // document.createElement 메서드 -> 새로운 Element 객체 생성
-    li.innerText = message.data; // 생성된 li요소의 텍스트 속성에 message 객체의 data 반영
-    messageList.append(li); // 생성된 li 요소를 home.pug의 ul 안에 추가
-}); // "message" 이벤트 -> 메시지 전달 : 메시지를 전달 받아서 콘솔에 출력
-
-socket.addEventListener("close", () => {
-    console.log("Disconnected from Server")
-}); // "close" 이벤트 -> 서버 오프라인 상태
-
-function handleSubmit(event){ 
-    // handleSubmit 함수 : 입력 폼에서 submit 이벤트가 발생했을 때 동작으로 등록
-    event.preventDefault(); 
-    // 이벤트를 취소하는 메소드 -> 이벤트가 제공하는 원래 기능을 사용하지 않으려고 할 때 사용
-    const input = messageForm.querySelector("input"); 
-    socket.send(makeMessage("new_message", input.value)); // 입력된 내용을 소켓을 이용해 전송
-    input.value = ""; // 전송 후 입력 필드의 내용을 삭제(초기화)
-}
-
-function handleNickSubmit(event){
-    // handleNickSubmit 함수 : Nick 입력 폼에서 submit 이벤트가 발생했을 때 동작으로 등록
+function handleRoomSubmit(event){ // input에 입력된 값을 읽고, 이를 소켓을 통해 서버에 전송
     event.preventDefault();
-    // 이벤트를 취소하는 메소드 -> 이벤트가 제공하는 원래 기능을 사용하지 않으려고 할 때 사용
-    const input = nickForm.querySelector("input");
-    socket.send(makeMessage("nickname", input.value)); // makeMessage함수 결과값 -> 소켓을 이용해 전송
-    input.value = ""; // 전송 후 입력 필드의 내용을 삭제(초기화)
+    const input = form.querySelector("input");
+    socket.emit("enter_room", input.value, () => {
+        // 첫번째 인자: 이벤트명, 두번째 인자: 서버에 전송할 데이터, 세번째 인자: 익명 함수
+        console.log("server is done!"); 
+    }); 
+    // socket.emit 메소드 -> 이벤트 발생 / JSON객체를 통한 문자열 변환을 하지 않아도 원활하게 데이터 전송이 이뤄짐.
+    input.value = "";
 }
 
-messageForm.addEventListener("submit", handleSubmit); 
-// submit 이벤트 발생 -> 콜백 함수(handleSubmit)에 이벤트 전달
-nickForm.addEventListener("submit", handleNickSubmit);
-// 닉네임 폼에서 닉네임 입력 -> submit 이벤트 발생 -> handleNickSubmit 함수 호출
+form.addEventListener("submit", handleRoomSubmit); // submit 이벤트 발생 -> handle
--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 import http from "http";
-import WebSocket from "ws";
+import SocketIO from "socket.io";
 import express from "express";
 
 const app = express();
@@ -10,36 +10,17 @@ app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (req, res) => res.render("home"));
 app.get("/*", (req, res) => res.redirect("/"));
 
-const handleListen = () => console.log("Listening on http://localhost:3000");
-const server = http.createServer(app); 
+const httpServer = http.createServer(app); 
 // Node.js에 기본으로 내장된 HTTP 패키지를 사용해서 express로 만든 서버 애플리케이션 제공
-const wss = new WebSocket.Server({ server });
-// 웹 소켓 서버를 생성하면서 여기에 HTTP서버를 전달
+// HTTP 서버: httpServer, 웹소켓 서버: wsServer
+const wsServer = SocketIO(httpServer); // wsServer를 만들기 위해 SocketIO에 httpServer를 넘겨줌
 
-const sockets = []; // 가짜 데이터베이스에서 활용할 배열 생성
-
-wss.on("connection", (socket) => {
-   sockets.push(socket); // 웹소켓 서버에 connection이벤트 발생때마다 배열에 생성된 소켓 추가
-   socket["nickname"] = "Anonymous" // 소켓이 생성되었을 때 모든 사용자의 닉네임으로 'anonymous' 설정
-   console.log("Connected to Browser"); 
-   socket.on("close", () => console.log("Disconnected from Browser"));
-   // close 이벤트 발생 -> 서버쪽 콘솔에 메시지 출력
-   socket.on("message", (msg) => { // message 이벤트 -> 콜백 함수는 message를 전달 받음
-      const message = JSON.parse(msg) // 문자열을 JSON으로 변환
-      switch(message.type){ // message.type의 값에 따라 어떤 코드를 사용할 것인지 결정
-         case "new_message": // 메시지의 내용인 payload를 전송
-            sockets.forEach(asocket => asocket.send(`${socket.nickname}:${message.payload}`));
-            // forEach메소드: 배열의 각 요소에 차례로 접근해서 구문을 실행해주는 메소드
-            break;
-         case "nickname":
-            socket["nickname"] = message.payload;
-            // 소켓에 nickname 키 추가 -> 추가된 키에 payload 대입
-            break;
-      }      
-   });  
+wsServer.on("connection", (socket) => {
+    socket.on("enter_room", (roomName) => console.log(roomName));
+    // socket.on -> 이벤트 핸들링 메소드
+    // 첫번째 인자 -> 이벤트 이름, 두번째 인자 -> 이벤트 핸들러 함수
+    // 매개변수 roomName -> 프론트엔드에서 emit메소드를 통해 보낸 객체가 전달됨.
 });
-// handleConection 함수를 선언한 다음에 on 메소드에 인자로 전달 
-// -> 같은 역할을 하는 익명함수를 만들어 on 메소드에 포함
-// connection 이벤트가 발생했을 때 소켓을 받는 다는 것을 직관적으로 표시
 
-server.listen(3000, handleListen);
+const handleListen = () => console.log("Listening on http://localhost:3000");
+httpServer.listen(3000, handleListen);
